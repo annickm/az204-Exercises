430a205915d11e66e99539e3fad6022e610fb7ae
annickm <38430347+annickm@users.noreply.github.com>
2021-03-06 14:45:39 +0000

    Add files via upload

---
+++ Skills2.2/InteractwithData/AppSettings.cs
@@ -0,0 +1,24 @@
+//C# .NET Core
+using Microsoft.Extensions.Configuration;
+
+namespace InteractwithData
+{
+    public class AppSettings
+    {
+        public string SourceASAConnectionString { get; set; }
+        public string SourceAccountName { get; set; }
+        public string SourceContainerName { get; set; }
+        public string DestinationASAConnectionString { get; set; }
+        public string DestinationAccountName { get; set; }
+        public string DestinationContainerName { get; set; }
+
+        public static AppSettings LoadAppSettings()
+        {
+            IConfigurationRoot configRoot = new ConfigurationBuilder()
+                .AddJsonFile("AppSettings.json", false)
+                .Build();
+            AppSettings appSettings = configRoot.Get<AppSettings>();
+            return appSettings;
+        }
+    }
+}
