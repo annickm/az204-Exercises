df091ac09c758b88bb0f3581eeee55fcbf00bf60
annickm <38430347+annickm@users.noreply.github.com>
2021-03-13 10:06:03 +0000

    Add files via upload
    Added files for the AuthorizationServer example

---
+++ Skills3.1/OAuth2/Skills31OAuth2/AuthorizationServer/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthorizeWebApplication.Startup))]
+namespace AuthorizeWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            ConfigureWebApi(app);
+        }
+    }
+}
