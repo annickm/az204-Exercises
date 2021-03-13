3398e912349b5a5185f69c88c55f0bf86fb32ef1
annickm <38430347+annickm@users.noreply.github.com>
2021-03-13 10:15:21 +0000

    Add files via upload
    Added folders

---
+++ Skills3.1/OAuth2/Skills31OAuth2/AuthorizationServer/Constants/Clients.cs
@@ -0,0 +1,26 @@
+﻿namespace AuthorizeWebApplication.Constants
+{
+
+    public class Clients
+    {
+        public static readonly Client Client1 = new Client
+        {
+            Id = "123456",
+            Secret = "abcedf",
+            RedirectUrl = Paths.AuthorizeCodeCallBackPath
+        };
+        public static readonly Client Client2 = new Client
+        {
+            Id = "78901",
+            Secret = "aasdsdef",
+            RedirectUrl = Paths.ImplicitGrantCallBackPath
+        };
+
+        public class Client
+        {
+            public  string Id { get; set; }
+            public string Secret { get; set; }
+            public string RedirectUrl { get; set; }
+        }
+    }
+}
---
+++ Skills3.1/OAuth2/Skills31OAuth2/AuthorizationServer/Constants/Paths.cs
@@ -0,0 +1,15 @@
+﻿namespace AuthorizeWebApplication.Constants
+{
+    public class Paths
+    {
+        public const string AuthorizationServerBaseAddress = "https://localhost:44372";
+        public const string ResourceServerBaseAddress = "https://localhost:44372";
+        public const string ImplicitGrantCallBackPath = "https://localhost:44372/Home/SignIn";
+        public const string AuthorizeCodeCallBackPath = "https://localhost:44372/Manage";
+        public const string AuthorizePath = "/OAuth/Authorize";
+        public const string TokenPath = "/OAuth/Token";
+        public const string LoginPath = "Account/Login";
+        public const string LogoutPath = "/Account/Logout";
+        public const string MePath = "/api/Me";
+    }
+}
